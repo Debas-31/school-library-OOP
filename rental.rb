86f04c06a461fb0cdebb060a6f325e0758c266f4
debas-31 <debas31@gmail.com>
2022-04-07 15:43:22 +0300

    Modify student class by adding method outside a constructor recommended by the code reviwer

--- rental.rb
+++ rental.rb
@@ -1,7 +1,7 @@
 class Rental
   attr_accessor :date, :person, :book
 
-  def initialize
+  def initialize(date, book, person)
     @date = date
     @person = person
     @book = book
--- student.rb
+++ student.rb
@@ -6,7 +6,11 @@ class Student < Person
   def initialize(age, name, parent_permission, classroom)
     super(age, name, parent_permission)
     @classroom = classroom
-    classroom.add_student(self)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
