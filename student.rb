d378e04633f0d51e5133e501206a7d8c38c1cba7
debas-31 <debas31@gmail.com>
2022-04-07 13:50:50 +0300

    Create many to many relation ship using intermidate class

--- student.rb
+++ student.rb
@@ -6,13 +6,7 @@ class Student < Person
   def initialize(age, name, parent_permission, classroom)
     super(age, name, parent_permission)
     @classroom = classroom
-  end
-
-  def classroom = (classroom)
-    @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
-      
-    end
+    classroom.add_student(self)
   end
 
   def play_hooky
