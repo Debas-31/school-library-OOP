a756f506fa1c4f9d3dc38245e4cbe6f99495fa1f
debas-31 <debas31@gmail.com>
2022-04-08 17:59:54 +0300

    Create person feature

--- app.rb
+++ app.rb
@@ -34,4 +34,51 @@ class App
     puts 'Database is empty! Add a person.' if @persons.empty?
     @persons.each { |person| puts "[#{person.class.name}] Name: #{person.name} Age: #{person.age} Id: #{person.id}" }
   end
+
+  def create_person
+    print 'To create a student, press 1, to create a teacher, press 2 :'
+    option = gets.chomp
+
+    case option
+    when '1'
+      create_student
+    when '2'
+      create_teacher
+    else
+      puts 'Invalid input. Try again'
+    end
+  end
+
+  def create_student
+    puts 'Creaate a new student'
+    print 'Enter name: '
+    name = gets.chomp
+    print 'Enter student age'
+    age = gets.chomp.to_i
+    print 'Has parent permission? [y/n]: '
+    parent_permission = gets.chomp.downcase
+    case parent_permission
+    when 'n'
+      Student.new(age, 'classroom', name, parent_permission: false)
+      @persons << student
+      puts "Student doesn't have parent permission, can't rent a books "
+    when 'y'
+      student = Student.new(age, 'classroom', name, parent_permission: true)
+      @persons << student
+      puts 'Student created successfully'
+    end
+  end
+
+  def create_teacher
+    puts 'Create a new teacher'
+    print 'Enter name: '
+    name = gets.chomp
+    print 'Enter teacher age'
+    age = gets.chomp.to_i
+    print 'Enter teacher specialization: '
+    specialization = gets.chomp
+    teacher = Teacher.new(age, specialization, name)
+    @person << teacher
+    puts 'Teacher created successfully'
+  end
 end
--- student.rb
+++ student.rb
@@ -4,7 +4,7 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(age, name, parent_permission, classroom)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
