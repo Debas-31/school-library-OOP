7d85937ab4a1fe63b94ce44260a7d49ea0dd9e75
debas-31 <debas31@gmail.com>
2022-04-11 16:32:03 +0300

    Fix linters error

--- console_options.rb
+++ console_options.rb
@@ -1,4 +1,4 @@
-class Console 
+class Console
   def start_console
     puts 'Welcome to my School Library!'
     until list_of_options
@@ -12,33 +12,33 @@ class Console
     end
   end
 
-    def list_of_options
-        puts 'Enter a number from the list of options: '
-        puts '1 - List all books'
-        puts '2 - List all people'
-        puts '3 - Create a person'
-        puts '4 - Create a book'
-        puts '5 - Create a rental'
-        puts '6 - List all rentals for a given person id'
-        puts '7 - Exit'
-      end
-      
-      def option(input)
-        case input
-        when '1'
-          list_all_books
-        when '2'
-          list_all_persons
-        when '3'
-          create_person
-        when '4'
-          create_book
-        when '5'
-          create_rental
-        when '6'
-          list_all_rentals
-        else
-          puts 'please enter a number between 1 and 7'
-        end
-      end
+  def list_of_options
+    puts 'Enter a number from the list of options: '
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+
+  def option(input)
+    case input
+    when '1'
+      list_all_books
+    when '2'
+      list_all_persons
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_all_rentals
+    else
+      puts 'please enter a number between 1 and 7'
+    end
+  end
 end
